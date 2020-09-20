3108ae6b198377ba45f4050c80c25a364b694f6f
Muhammad Arifin Ardi <47668013+ardirjs@users.noreply.github.com>
2020-09-21 04:03:48 +0700

    Add files via upload

---
+++ node_mcu/node_mcu.ino
@@ -0,0 +1,57 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+#include <SoftwareSerial.h>
+
+#define SSID "rjs"
+#define PASS "mboh opo"
+#define HOST "kontroler-mcu.firebaseio.com"
+#define AUTH "JLpe1vjuPmuihvhNmldtBemCMDfdX5NjQNQuNwuV"
+
+SoftwareSerial usart(12, 14);
+
+void setup() {
+  usart.begin(9600);
+  Serial.begin(9600);
+  WiFi.begin(SSID, PASS);
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("Connected: ");
+  Serial.println(WiFi.localIP());
+  Firebase.begin(HOST, AUTH);
+  delay(5000);
+  Serial.println("Start");
+  usart.print("Start");
+}
+
+void loop() {
+  if (usart.available()) {
+    String json = usart.readString();
+    Serial.print("Recv:\t");
+    Serial.println(json);
+
+    StaticJsonBuffer <200> doc;
+    JsonObject &root = doc.parseObject(json);
+    if (!root.success()) {
+      return;
+    }
+
+    Firebase.setFloat("temperature", root["data"][0]);
+    Firebase.setFloat("humidity", root["data"][1]);
+
+    JsonObject &roots = doc.createObject();
+    JsonArray &data = roots.createNestedArray("data");
+    data.add(Firebase.getFloat("setTemperature"));
+    data.add(Firebase.getFloat("setHumidity"));
+    data.add(Firebase.getFloat("setInterval"));
+
+    String dataJson;
+    roots.printTo(dataJson);
+    Serial.print("Send:\t");
+    Serial.println(dataJson);
+    usart.print(dataJson);
+  }
+}
---
+++ pro_mini/pro_mini.ino
@@ -0,0 +1,117 @@
+#include <DHT.h>
+#include <ArduinoJson.h>
+#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+
+#define FAN 7
+#define LAMP 8
+#define POMPA A2
+#define UNKNOWNS A3
+
+DHT dht(9, DHT22);
+SoftwareSerial usart(6, 5);
+LiquidCrystal lcd(A1, A0, 13, 12, 11, 10);
+
+static float h = 0, t = 0;
+static int setTemperature, setHumidity, setInterval;
+
+void setup() {
+  dht.begin();
+  lcd.begin(16, 2);
+  usart.begin(9600);
+  Serial.begin(9600);
+  int num = 4, bits[num] = {FAN, LAMP, POMPA, UNKNOWNS};
+  for (int i = 0; i < num; i ++) {
+    pinMode(bits[i], true);
+    digitalWrite(bits[i], true);
+  }
+  lcd.clear();
+  char *name = "ARIF SETIAWAN 16";
+  lcd.setCursor(0, 0);
+  for (int i = 0; i < 16; i ++) {
+    lcd.print(name[i]);
+    delay(0);
+    if (name[i] != ' ') {
+      delay(150);
+    }
+  }
+  delay(1500);
+  lcd.clear();
+  delay(1000);
+  lcd.setCursor(0, 1); lcd.print("Connecting...!!!");
+}
+
+void loop() {
+  if (usart.available()) {
+    String json = usart.readString();
+    Serial.print("Recv:\t");
+    Serial.println(json);
+
+    h = dht.readHumidity();
+    t = dht.readTemperature();
+    if (isnan(h) || isnan(t)) {
+      return;
+    }
+
+    StaticJsonBuffer <200> doc;
+    JsonObject &root = doc.createObject();
+    JsonArray &data = root.createNestedArray("data");
+    data.add(t);
+    data.add(h);
+
+    String dataJson;
+    root.printTo(dataJson);
+    Serial.print("Send:\t");
+    Serial.println(dataJson);
+    usart.print(dataJson);
+
+    JsonObject &roots = doc.parseObject(json);
+    if (!roots.success()) {
+      return;
+    }
+
+    setTemperature = roots["data"][0];
+    setHumidity = roots["data"][1];
+    setInterval = roots["data"][2];
+
+    static boolean clear = true;
+    if (clear) {
+      lcd.clear();
+      clear = false;
+    }
+
+    lcd.setCursor(0, 0); lcd.print("T:     C");
+    lcd.setCursor(6, 0); lcd.print((char)223);
+    lcd.setCursor(9, 0); lcd.print("H:    %");
+    lcd.setCursor(2, 0); lcd.print(t, 1);
+    lcd.setCursor(11, 0); lcd.print(h, 1);
+
+    lcd.setCursor(0, 1); lcd.print("ST:");
+    lcd.setCursor(3, 1); lcd.print(setTemperature);
+    lcd.setCursor(6, 1); lcd.print("SH:");
+    lcd.setCursor(9, 1); lcd.print(setHumidity);
+    lcd.setCursor(12, 1); lcd.print("IN:");
+    lcd.setCursor(15, 1); lcd.print(setInterval);
+  }
+
+  if (t && h) {
+    if ((float)t > (float)(setTemperature + setInterval)) {
+      digitalWrite(FAN, false);
+      digitalWrite(LAMP, true);
+    }
+    else if ((float)t < (float)(setTemperature - setInterval)) {
+      digitalWrite(FAN, true);
+      digitalWrite(LAMP, false);
+    }
+    else {
+      digitalWrite(FAN, true);
+      digitalWrite(LAMP, true);
+    }
+    if ((float)h < (float)setHumidity) {
+      digitalWrite(POMPA, false);
+    }
+    else {
+      digitalWrite(POMPA, true);
+    }
+  }
+}
